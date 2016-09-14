43b4505fb342002399002635884fd05862c2581e
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-09-14 12:06:34 -0300

    level2/2

--- modules/app.js
+++ modules/app.js
@@ -11,6 +11,11 @@
             price: 2.95,
             description: 'Some gems have hidden qualities beyond their luster,' 
             + 'beyond their shine... Dodeca is one of those gems',
+            images: [
+                {
+                    full: "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-06.gif"
+                }
+            ],
             canPurchase: true,
             soldOut: true
         },
@@ -18,8 +23,28 @@
             name: 'Pentagonal Gem',
             price: 5.95,
             description: "...",
+            images: [
+                {
+                    full: "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-01.gif"
+                }
+            ],
             canPurchase: false
         }
     ]
 
+    app.controller("PanelController", function () {
+        this.tab = 1;
+
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        };
+
+        this.isSelected = function (checktab) {
+            return this.tab === checktab;
+        };
+    });
+})();
+
+
+(function () { 
 })();
