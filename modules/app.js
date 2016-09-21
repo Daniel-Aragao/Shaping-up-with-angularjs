056a74b5cfe04883f5d4a1570c90407e1da6aaa3
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-09-21 12:01:26 -0300

    end

--- modules/app.js
+++ modules/app.js
@@ -1,49 +1,15 @@
 (function () {
-    var app = angular.module('store', []);
+    var app = angular.module('store', ['store-products']);
 
-    app.controller('StoreController', function () {
-        this.products = gems;
-    });
+    app.controller('StoreController', ['$http',function ($http) {
+        var store = this;
 
-    var gems = [
-        {
-            name: 'Dodecahedron',
-            price: 2.95,
-            description: 'Some gems have hidden qualities beyond their luster,' 
-            + 'beyond their shine... Dodeca is one of those gems',
-            images: [
-                {
-                    full: "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-06.gif"
-                }
-            ],
-            reviews: [
-                {
-                    stars: 5,
-                    body: "I love this product!",
-                    author: "joe@thomas.com"
-                },
-                {
-                    stars: 1,
-                    body: "This product sucks",
-                    author: "tim@hater.com"
-                }
-            ],
-            canPurchase: true,
-            soldOut: true
-        },
-        {
-            name: 'Pentagonal Gem',
-            price: 5.95,
-            description: "...",
-            images: [
-                {
-                    full: "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-01.gif"
-                }
-            ],
-            canPurchase: false
-        }
-    ]
+        store.products = [];
 
+        $http.get('../products.json').success(function (data) {
+           store.products = data; 
+        });
+    }]);
 
     app.controller("ReviewController", function () {
         this.review = {};
@@ -54,33 +20,6 @@
         }
     });
 
-    app.directive("productTitle", function () {
-        return {
-            restrict: 'E', // E from Element
-            templateUrl: 'product-title.html'
-        }; 
-    });
-
-    app.directive("productPanels", function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'product-panels.html',
-            controller: function () {
-                this.tab = 1;
-
-                this.selectTab = function (setTab) {
-                    this.tab = setTab;
-                };
-
-                this.isSelected = function (checktab) {
-                    return this.tab === checktab;
-                };
-            },
-            controllerAs: 'panels'
-        } 
-    });
-    /*app.controller("PanelController", function () {
-
-    });*/
+    
 
 })();
